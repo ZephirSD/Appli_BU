198f0ce4a5981a1def503c08163ed8b98812967a
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-11-17 16:23:35 +0400

    Création de la page de connexion

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    <form method="POST" action="login.php">
+        <label>Login</label>
+        <input type="text" name="username"/>
+        <label>Mot de passe</label>
+        <input type="password" name="password"/>
+        <button type="submit" name="btnConnexion">Connecter</button>
+    </form>
+</body>
+</html>
