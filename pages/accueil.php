b6afb950b315cd1c9f1aee8511b3d58e61fa41e9
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-11-18 11:38:28 +0400

    Création de la page d'accueil

---
+++ pages/accueil.php
@@ -0,0 +1,30 @@
+<?php
+    session_start();
+    if (isset($_SESSION['id']) && empty($_SESSION['id'])) {
+        header('location:http://localhost/Appli_BU/');
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+
+<body>
+    <span>
+        <form method="POST" action="../logout.php">
+            <button type="submit" name="btnDeco">Deconnecter</button>
+        </form>
+    </span>
+    <span><?php echo ($_SESSION['username']) ?></span>
+    <a href="utilisateurs/utilisateurs.php">Utilisateurs</a>
+    <a href="postes/postes.php">Postes</a>
+    <a href="reservations/reservations.php">Resservations</a>
+</body>
+
+</html>
