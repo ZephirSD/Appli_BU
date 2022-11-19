aef977e4d2cd9140ad5aedc5e70a9af188ac534e
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-11-19 10:23:49 +0400

    Ajout des fonctionnalités permettant de modifier la disponibilité des postes et des utilisateurs

--- pages/reservations/creer/requete_creer_reservation.php
+++ pages/reservations/creer/requete_creer_reservation.php
@@ -8,6 +8,8 @@
             $dispoUtil = $_POST['dispoUtilis'];
             $dtDtRsv = $_POST['dtDebutReserv'];
             $dtFinRsv = $_POST['dtFinReserv'];
+            $attrValide = "1";
+            $posteValide = "0";
             $sql = "INSERT INTO reservation (numero, poste_attr, utilisateur_attr, date_debut, date_fin) VALUES (:numero, :posteAttr, :utilisAttr, :dtDebut, :dtFin)";
             $stmt = $db->prepare($sql);
             $stmt->bindParam(':numero', $numReserv);
@@ -15,7 +17,18 @@
             $stmt->bindParam(':utilisAttr', $dispoUtil);
             $stmt->bindParam(':dtDebut', $dtDtRsv);
             $stmt->bindParam(':dtFin', $dtFinRsv);
-            $stmt->execute();
+            if($stmt->execute() == true){
+                $sqlUser = "UPDATE utilisateur SET Attribution = :Attribution WHERE Id = :Id";
+                $stmtUser = $db->prepare($sqlUser);
+                $stmtUser->bindParam(':Attribution', $attrValide);
+                $stmtUser->bindParam(':Id', $dispoUtil);
+                $stmtUser->execute();
+                $sqlPoste = "UPDATE poste SET Disponible = :Disponible WHERE Id = :Id";
+                $stmtPoste = $db->prepare($sqlPoste);
+                $stmtPoste->bindParam(':Disponible', $posteValide);
+                $stmtPoste->bindParam(':Id', $dispoPoste);
+                $stmtPoste->execute();
+            }
             header('location:http://localhost/Appli_BU/pages/reservations/reservations.php');
         }
         catch(PDOException $e){
