03e47f757402b3ecc5426e33a72734e91e5bc21c
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-11-19 10:16:27 +0400

    Suppression de la 3ème requête

--- pages/reservations/creer/requete_recup_info_reserv.php
+++ pages/reservations/creer/requete_recup_info_reserv.php
@@ -8,18 +8,10 @@
     }catch (PDOException $e){
         echo $e->getMessage();
     } 
-    $sql2 = 'SELECT * FROM utilisateur ORDER BY Nom';
+    $sql2 = 'SELECT * FROM utilisateur WHERE Attribution = "0" ORDER BY Nom';
     try{
         $stmtRec2 = $db->query($sql2);    
         $stmtRec2->execute();
-    }catch (PDOException $e){
-        echo $e->getMessage();
-    } 
-    $sql3 = 'SELECT utilisateur_attr FROM reservation';
-    try{
-        $stmtRec3 = $db->query($sql3);    
-        $stmtRec3->execute();
-        $result = $stmtRec3->fetchAll();
     }catch (PDOException $e){
         echo $e->getMessage();
     } 
